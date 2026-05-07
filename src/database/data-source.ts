eda3a5576fe994c793835c1531e599a345ac77fd
AbuJubair7 <jubaircode7@gmail.com>
2026-05-07 11:01:49 +0600

    Smaple .env added

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -9,10 +9,7 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  url:
-    process.env.NODE_ENV === "production"
-      ? process.env.DATABASE_URL
-      : undefined,
+  url: process.env.DATABASE_URL,
   host: process.env.DB_HOST || "localhost",
   port: parseInt(process.env.DB_PORT || "5432"),
   username: process.env.DB_USERNAME || "postgres",
